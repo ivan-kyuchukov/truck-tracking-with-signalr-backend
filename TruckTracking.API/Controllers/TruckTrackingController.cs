d84e827efd1a5757a05bb7194a17cde1ed629268
ivan-kyuchukov <ivan.kyuchukov@accedia.com>
2020-04-10 19:56:20 +0300

    initial

---
+++ TruckTracking.API/Controllers/TruckTrackingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TruckTracking.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TruckTrackingController : ControllerBase
+    {
+        private readonly ILogger<TruckTrackingController> _logger;
+
+        public TruckTrackingController(ILogger<TruckTrackingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "test";
+        }
+    }
+}
---
+++ TruckTracking.API/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TruckTracking.API.Hubs;
+
+namespace TruckTracking.API
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy(name: MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder
+                        .WithOrigins("http://127.0.0.1:5500")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                }));
+
+            services.AddControllers();
+
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
+            });
+        }
+    }
+}
